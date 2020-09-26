7c3c43f41fecd2a8e79bd6b7328a7fc23b470060
DevTaehong <minth1123@icloud.com>
2020-09-26 11:13:06 -0300

    FInalInclassExercise

--- Exercises/HocketTeam.py
+++ Exercises/HocketTeam.py
@@ -3,6 +3,7 @@ def main():
     #INPUT
     NameHockey = input("Please enter the name of a hockey team: ")
     
+    #PROCESSING
     Wins = 10
     Losess = 5
     Percentage = Wins / (Wins + Losess)
@@ -10,6 +11,7 @@ def main():
     Ratio1 = Wins / Ratio
     Ratio2 = Losess / Ratio
 
+    #OUTPUT
     print("{0} have {1} wins and {2} losses, with a win percentage of {3:.4f} and win-loss ratio is {4:.0f} : {5:.0f} ".format(NameHockey, Wins, Losess, Percentage, Ratio1, Ratio2 ))
 
 
