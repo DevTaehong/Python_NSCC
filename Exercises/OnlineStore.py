8ebea743d42dee7da095615dcc6cb654fd02ad99
DevTaehong <minth1123@icloud.com>
2020-10-19 19:43:06 -0300

    InClassExerciseDoneLast

--- Exercises/OnlineStore.py
+++ Exercises/OnlineStore.py
@@ -13,14 +13,20 @@ def main(): #<-- Don't change this line!
     #User inputs - which country and order total
     orderAmount = float(input("Enter the amount of your order: $"))
     selectedCountry = input("What country are you from? ")
-
+    
     #If the user is from Canada, ask which province
     if selectedCountry.lower() == "canada":
         province = input("Which province are you from? ")
+
+        #If the order was placed in Canada calculate tax based on the province
+        #Alberta charge 5% GST
         if province.lower() == "alberta":
             totalWithTax = orderAmount * 0.05
+        #Ontario, NB, NS charge 15%
         elif province.lower() == "ontario" or province.lower() == "new brunswick" or province.lower() == "nova scotia":
             totalWithTax = orderAmount * 0.15
+        #All other province charge 11%
+        #Telling the user total with taxes for their order
         else:
             totalWithTax = orderAmount * 0.11
         print("Your total with taxes for your order is ${0:.2f}".format(totalWithTax + orderAmount))
@@ -28,18 +34,6 @@ def main(): #<-- Don't change this line!
     #If the order is from outside Canada do not charge any taxes
     else:
         print("Your total order is {0:.2f}".format(orderAmount))
-
-    
-
-    #If the order was placed in Canada calculate tax based on the province
-        #Alberta charge 5% GST
-        
-        #Ontario, NB, NS charge 15%
-
-        #All other province charge 11%
-
-    #Telling the user total with taxes for their order
-
 
     #Your code ends on the line above
 
