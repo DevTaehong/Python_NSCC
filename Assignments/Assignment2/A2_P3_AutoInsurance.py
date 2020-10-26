7d23a116b232f537c3582991c4d869f74a945713
DevTaehong <minth1123@icloud.com>
2020-10-26 01:06:33 -0300

    A2Program3Done

--- Assignments/Assignment2/A2_P3_AutoInsurance.py
+++ Assignments/Assignment2/A2_P3_AutoInsurance.py
@@ -1,16 +1,50 @@
 #Don't forget to rename this file after copying the template
 #for a new program!
 """
-Student Name:    
-Program Title:  
-Description:    
+Student Name:   Taehong Min
+Program Title:  Auto Insurance
+Description:    Writing a program that computes monthly insurance
 """
 
 def main(): #<-- Don't change this line!
     #Write your code below. It must be indented!
+    fistAge = 15
+    secondAge = 25
+    thirdAge = 40
+    lastAge = 70
+    monthlyPrice = 0.0
+    monthOfYear = 12
+    firstMalePriceRate = 0.25
+    secondMalePriceRate = 0.17
+    lastMalePriceRate = 0.10
+    firstFemalePriceRaTe = 0.20
+    secondFemalePriceRaTe = 0.15
+    lastFemalePriceRaTe = 0.10
+    
+    #INPUT
+    gender = input("Are you 'Male' or 'Female': ").lower()
+    age = int(input("Enter your age: "))
+    price = float(input("Enter the purchase price of the vehicle: "))
+    
+    #PROCESSING
+    if gender == "male":
+        if age >= fistAge and age < secondAge:
+            monthlyPrice =  price * firstMalePriceRate / monthOfYear
+        elif age >= secondAge and age < thirdAge:
+            monthlyPrice =  price * secondMalePriceRate / monthOfYear
+        elif age >= thirdAge and age < lastAge:
+            monthlyPrice =  price * lastMalePriceRate / monthOfYear
 
+    elif gender == "female":
+        if age >= fistAge and age < secondAge:
+            monthlyPrice =  price * firstFemalePriceRaTe / monthOfYear
+        elif age >= secondAge and age < thirdAge:
+            monthlyPrice =  price * secondFemalePriceRaTe / monthOfYear
+        elif age >= thirdAge and age < lastAge:
+            monthlyPrice =  price * lastFemalePriceRaTe / monthOfYear
 
-
+    #OUTPUT
+    print("Your monthly insurance will be ${0:.2f}".format(monthlyPrice))
 
     #Your code ends on the line above
 
